1ffc45cd47d41979dff3aaba518fd990976a9fd3
Mark Suscens <mark.suscens@gmail.com>
2021-04-25 09:30:29 +0100

    Allow approvers to approve a transaction request

--- MultiSig Wallet Project/MultiSigWallet.sol
+++ MultiSig Wallet Project/MultiSigWallet.sol
@@ -1,3 +1,4 @@
+pragma solidity 0.7.5;
 pragma abicoder v2;
 
 import "./MultiOwnable.sol";
@@ -20,14 +21,14 @@ contract MultiSigWallet is MultiOwnable, Approvable {
     
     TxRequest[] internal _pendingApprovals;          // array index is request id
     mapping (uint => address) internal _txRequestors;
+    mapping (address => mapping (uint => bool)) internal _txApprovals;  // approver => (requestId => approved?)
     
-    // - openRequestsByOwner : mapping owner's address -> array of transfer request ids (uint[]) 
-    
-    // - sentTransactions ? (possible nice to have, to keep a record of who approved sent transactions)
-    // - rejectedTransactions ? (possible nice to have, similar to above - keep a record of who rejected proposed Txs)
-    
+    event TransferSent(address to, uint amount); 
+
     
     // FUNCTIONS
+    // Public & External 
+    
     constructor(address[] memory owners, uint minTxApprovals)
         MultiOwnable(owners)
         Approvable(owners, minTxApprovals)
@@ -35,18 +36,10 @@ contract MultiSigWallet is MultiOwnable, Approvable {
         _walletCreator = msg.sender;
     }
     
+    
     function deposit() external payable returns (uint amountDeposited) {
         require (msg.value > 0, "No funds sent to deposit!");
         return(msg.value);
-    }
-    
-    // Useful functions for testing
-    function getWalletBalance() public view returns (uint balance) {
-        return address(this).balance;
-    }
-    
-    function getWalletCreator() public view returns (address) {
-        return _walletCreator;
     }
     
         
@@ -61,25 +54,63 @@ contract MultiSigWallet is MultiOwnable, Approvable {
         return newRequest.id;
     }
     
-    function cancelTransferRequest(uint requestId) public {
+
+    function approveTransferRequest(uint requestId) public onlyAnApprover(msg.sender) {
+        // Adds an approval vote to pending request, and triggers transfer if approval vote threshold reached
         require(requestId < _pendingApprovals.length, "No such request id!");
-        require(_txRequestors[requestId] == msg.sender, "Not owner of tx request!");
+        require(_txApprovals[msg.sender][requestId] != true, "You've already approved!");
+
+        _txApprovals[msg.sender][requestId] = true;
+        _pendingApprovals[requestId].approvals++;
         
-        delete _pendingApprovals[requestId];
-        delete _txRequestors[requestId];
+        if (_pendingApprovals[requestId].approvals >= _minApprovals) {
+            
+            address sendTo = _pendingApprovals[requestId].recipient;
+            uint amountInWei = _pendingApprovals[requestId].amount;
+
+            _deleteTransferRequest(requestId);
+            
+            // Make the transfer
+            // *** TODO ***
+            
+            // Emit a transferSent event 
+            emit TransferSent(sendTo, amountInWei);
+        }
+        // mapping (address => mapping (uint => bool)) internal _txApprovals;  // approver => (requestId => approved?)
     }
     
-    function numberTransferRequests() public view returns (uint) {
-        return _pendingApprovals.length;    // note includes cancelled requests
+    
+    function cancelTransferRequest(uint requestId) public onlyAnOwner(msg.sender) {
+        require(requestId < _pendingApprovals.length, "No such request id!");
+        require(_txRequestors[requestId] == msg.sender, "Not creator of tx request!");
+        
+        _deleteTransferRequest(requestId);
     }
     
+    
+    // Useful public functions for testing during development
     function getTransferRequest(uint id) public view returns (TxRequest memory transferRequest) {
         return _pendingApprovals[id];
     }
     
+    function getWalletBalance() public view returns (uint balance) {
+        return address(this).balance;
+    }
     
-    // - accceptTransferRequest(index) onlyOwners : Adds an approval vote to pending request, and triggers transfer if approval vote threshold reached
-    // - declineTransferRequest(index) onlyOwners : Adds a not approved vote to pending request, and rejects request if approval vote threshold can no longer be reached
+    function getWalletCreator() public view returns (address) {
+        return _walletCreator;
+    }
     
+    function totalTransferRequests() public view returns (uint) {
+        return _pendingApprovals.length;    // note includes deleted requests (i.e cancelled and approved requests)
+    }
+    
+    
+    // Internal and Private functions
+    
+    function _deleteTransferRequest(uint requestId) internal {
+        delete _pendingApprovals[requestId];
+        delete _txRequestors[requestId];
+    }
     
 }
